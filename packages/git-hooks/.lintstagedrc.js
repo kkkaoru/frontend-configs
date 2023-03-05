aef6b4c9204b65b56f8ef40fc5cd42b91f116500
kaoru.ohtaka <ockacru+git@gmail.com>
2023-03-05 22:47:15 +0900

    feat(prettier-config): add prettier config

--- packages/git-hooks/.lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  '*.{ts,tsx}': (files) => [
-    `eslint --max-warnings=0 --fix --no-ignore ${files.join(' ')}`,
-    `prettier -cw ${files.join(' ')}`,
-    `jest --passWithNoTests --findRelatedTests --collectCoverageFrom=${files.join(' ')}`,
-  ],
-  '*.js': ['prettier -cw'],
-  '*.yml': ['prettier -cw'],
-};
