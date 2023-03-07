b973d03eba165966cb56d9997bb3d9d19f621cf1
kaoru.ohtaka <ockacru+git@gmail.com>
2023-03-08 00:53:25 +0900

    feat(eslint-config): apply vitest in eslint

--- packages/eslint-config/overrides/index.js
+++ packages/eslint-config/overrides/index.js
@@ -3,6 +3,14 @@ const storiesTsxOverrides = require('./stories-tsx');
 const sliceTsOverrides = require('./slice-ts');
 const testTsxTsOverrides = require('./test-ts-tsx');
 const pageTsxOverrides = require('./page-tsx');
+const vitestConfigTsOverrides = require('./vitest-config-ts');
 
 /** @type import('eslint').Linter.ConfigOverride[] */
-module.exports = [tsxOverrides, storiesTsxOverrides, testTsxTsOverrides, sliceTsOverrides, pageTsxOverrides];
+module.exports = [
+  tsxOverrides,
+  storiesTsxOverrides,
+  testTsxTsOverrides,
+  sliceTsOverrides,
+  pageTsxOverrides,
+  vitestConfigTsOverrides,
+];
---
+++ packages/eslint-config/overrides/vitest-config-ts.js
@@ -0,0 +1,7 @@
+/** @type import('eslint').Linter.ConfigOverride */
+module.exports = {
+  files: ['vitest.config.ts'],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+  },
+};
